f90410043042bb02b41348c322a2d09cb337fc15
Hermes Pique <hermespique@gmail.com>
2014-08-08 19:15:09 +0200

    Add cache name

--- Haneke/MemoryCache.swift
+++ Haneke/MemoryCache.swift
@@ -11,13 +11,16 @@ import UIKit
 
 public class MemoryCache {
     
+    let name : String
+    
     let cache = NSCache()
     
     let memoryWarningObserver : NSObjectProtocol?
     
-    public init () {
-        let notifications = NSNotificationCenter.defaultCenter()
+    public init(_ name : String) {
+        self.name = name
         
+        let notifications = NSNotificationCenter.defaultCenter()
         // Using block-based observer to avoid subclassing NSObject
         memoryWarningObserver = notifications.addObserverForName(UIApplicationDidReceiveMemoryWarningNotification,
             object: nil,
