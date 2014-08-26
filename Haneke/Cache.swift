88eae277adadaf20b39819e9ee59d0bbc8c566dc
Hermes Pique <hermespique@gmail.com>
2014-08-27 01:16:39 +0200

    First go at formats

--- Haneke/Cache.swift
+++ Haneke/Cache.swift
@@ -10,20 +10,19 @@ import Foundation
 import UIKit
 import Haneke
 
+let OriginalFormatName = "original"
+
 public class Cache {
     
     let name : String
-    
-    let memoryCache = NSCache()
-    
-    let diskCache : DiskCache
     
     let memoryWarningObserver : NSObjectProtocol?
     
     public init(_ name : String) {
         self.name = name
-        self.diskCache = DiskCache(self.name)
 
+        self.registerFormat(Format(OriginalFormatName))
+        
         let notifications = NSNotificationCenter.defaultCenter()
         // Using block-based observer to avoid subclassing NSObject
         memoryWarningObserver = notifications.addObserverForName(UIApplicationDidReceiveMemoryWarningNotification,
@@ -40,24 +39,45 @@ public class Cache {
         notifications.removeObserver(memoryWarningObserver!, name: UIApplicationDidReceiveMemoryWarningNotification, object: nil)
     }
     
-    public func setImage (image: UIImage, _ key: String) {
-        memoryCache.setObject(image, forKey: key)
-        // Image data is sent as @autoclosure to be executed in the disk cache queue.
-        diskCache.setData(image.hnk_data(), key: key)
+    public func setImage (image: UIImage, _ key: String, formatName : String = OriginalFormatName) {
+        if let (_, memoryCache, diskCache) = self.formats[formatName] {
+            memoryCache.setObject(image, forKey: key)
+            // Image data is sent as @autoclosure to be executed in the disk cache queue.
+            diskCache.setData(image.hnk_data(), key: key)
+        }
     }
     
-    public func fetchImage (key : String) -> UIImage! {
-        return memoryCache.objectForKey(key) as UIImage!
+    public func fetchImage (key : String, formatName : String = OriginalFormatName) -> UIImage? {
+        if let (_, memoryCache, diskCache) = self.formats[formatName] {
+            return memoryCache.objectForKey(key) as UIImage!
+        }
+        return nil
     }
 
-    public func removeImage(key : String) {
-        memoryCache.removeObjectForKey(key)
-        diskCache.removeData(key)
+    public func removeImage(key : String, formatName : String = OriginalFormatName) {
+        if let (_, memoryCache, diskCache) = self.formats[formatName] {
+            memoryCache.removeObjectForKey(key)
+            diskCache.removeData(key)
+        }
     }
     
     // MARK: Notifications
     
     func onMemoryWarning() {
-        memoryCache.removeAllObjects()
+        for (_, (_, memoryCache, _)) in self.formats {
+            memoryCache.removeAllObjects()
+        }
     }
+    
+    // MARK: Formats
+
+    var formats : [String : (Format, NSCache, DiskCache)] = [:]
+    
+    public func registerFormat(format : Format) {
+        let name = self.name
+        let memoryCache = NSCache()
+        let diskCache = DiskCache(name)
+        self.formats[format.name] = (format, memoryCache, diskCache)
+    }
+    
 }
---
+++ Haneke/Format.swift
@@ -0,0 +1,19 @@
+//
+//  Format.swift
+//  Haneke
+//
+//  Created by Hermes Pique on 8/27/14.
+//  Copyright (c) 2014 Haneke. All rights reserved.
+//
+
+import Foundation
+
+public class Format {
+    
+    public let name : String
+    
+    public init(_ name : String) {
+        self.name = name
+    }
+    
+}
