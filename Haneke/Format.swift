40f260138a4d487e327c075e5830cc802890be3b
Oriol Blanc Gimeno <oriolblanc@gmail.com>
2014-08-30 00:11:17 +0200

    Add some format variables

--- Haneke/Format.swift
+++ Haneke/Format.swift
@@ -8,12 +8,27 @@
 
 import Foundation
 
+public enum ScaleMode {
+    case ScaleModeFill, ScaleModeAspectFit, ScaleModeAspectFill, ScaleModeNone
+}
+
 public struct Format {
     
     public let name : String
+
+    public var allowUpscaling : Bool = false
+    
+    public var compressionQuality : Float = 1.0
+
+    public let size : CGSize = CGSizeZero
+    
+    public var scaleMode: ScaleMode = .ScaleModeNone
     
     public init(_ name : String) {
         self.name = name
     }
     
+    public func resizedImageFromImage(originalImage: UIImage) -> UIImage {
+        return originalImage
+    }
 }
