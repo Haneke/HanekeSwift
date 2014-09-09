f5b37e468b15d67a61d8911601b16e2e40d7c13f
Hermes Pique <hermespique@gmail.com>
2014-09-09 17:30:43 +0200

    Fix isEqualPixelByPixel

--- HanekeTests/UIImage+Test.swift
+++ HanekeTests/UIImage+Test.swift
@@ -10,14 +10,25 @@ import UIKit
 
 extension UIImage {
     
-    func isEqualPixelByPixel(theOtherImage: UIImage!) -> Bool {
-        let imageData = UIImagePNGRepresentation(self)
-        let theOtherImageData = UIImagePNGRepresentation(theOtherImage)
-        
+    func isEqualPixelByPixel(theOtherImage: UIImage) -> Bool {
+        let imageData = self.normalizedData()
+        let theOtherImageData = theOtherImage.normalizedData()
         return imageData.isEqualToData(theOtherImageData)
     }
     
-    class func imageWithColor(color: UIColor, _ size: CGSize = CGSize(width: 1, height: 1), _ opaque: Bool = true) -> UIImage{
+    func normalizedData() -> NSData {
+        let pixelSize = CGSize(width : self.size.width * self.scale, height : self.size.height * self.scale)
+        NSLog(NSStringFromCGSize(pixelSize))
+        UIGraphicsBeginImageContext(pixelSize)
+        self.drawInRect(CGRect(x: 0, y: 0, width: pixelSize.width, height: pixelSize.height))
+        let drawnImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        let provider = CGImageGetDataProvider(drawnImage.CGImage)
+        let data = CGDataProviderCopyData(provider)
+        return data
+    }
+    
+    class func imageWithColor(color: UIColor, _ size: CGSize = CGSize(width: 1, height: 1), _ opaque: Bool = true) -> UIImage {
         UIGraphicsBeginImageContextWithOptions(size, opaque, 0)
         let context = UIGraphicsGetCurrentContext()
         CGContextSetFillColorWithColor(context, color.CGColor)
