fd09e8c99245e9bfd42b19bf42aa537e152831f1
Hermes Pique <hermespique@gmail.com>
2014-09-12 20:23:44 +0200

    NSHTTPURLResponse extension

---
+++ Haneke/NSHTTPURLResponse+Haneke.swift
@@ -0,0 +1,26 @@
+//
+//  NSHTTPURLResponse+Haneke.swift
+//  Haneke
+//
+//  Created by Hermes Pique on 9/12/14.
+//  Copyright (c) 2014 Haneke. All rights reserved.
+//
+
+import Foundation
+
+extension NSHTTPURLResponse {
+    
+    func hnk_validateLengthOfData(data : NSData) -> Bool {
+        let expectedContentLength = self.expectedContentLength;
+        if (expectedContentLength > -1) {
+            let dataLength = data.length;
+            if (Int64(dataLength) < expectedContentLength) {
+                return true
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+}
