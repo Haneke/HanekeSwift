95a8758c8c6a915870263c1e1038b598813121be
Oriol Blanc Gimeno <oriolblanc@gmail.com>
2014-09-18 19:23:41 +0200

    merge develop

---
+++ Haneke/NSHTTPURLResponse+Haneke.swift
@@ -0,0 +1,22 @@
+//
+//  NSHTTPURLResponse+Haneke.swift
+//  Haneke
+//
+//  Created by Hermes Pique on 9/12/14.
+//  Copyright (c) 2014 Haneke. All rights reserved.
+//
+
+import Foundation
+
+extension NSHTTPURLResponse {
+    
+    func hnk_validateLengthOfData(data : NSData) -> Bool {
+        let expectedContentLength = self.expectedContentLength;
+        if (expectedContentLength > -1) {
+            let dataLength = data.length;
+            return Int64(dataLength) >= expectedContentLength
+        }
+        return true
+    }
+    
+}
