aa405f769c0dca5b29b484eb795699b6518b7375
Hermes Pique <hermespique@gmail.com>
2014-11-10 05:15:55 +0000

    Move logging into a separate file

--- Haneke/Haneke.swift
+++ Haneke/Haneke.swift
@@ -53,32 +53,3 @@ func errorWithCode(code : Int, #description : String) -> NSError {
     let userInfo = [NSLocalizedDescriptionKey: description]
     return NSError(domain: HanekeGlobals.Domain, code: code, userInfo: userInfo)
 }
-
-struct Log {
-    
-    private static let Tag = "[HANEKE]"
-    
-    private enum Level : String {
-        case Debug = "[DEBUG]"
-        case Error = "[ERROR]"
-    }
-    
-    private static func log(level : Level, @autoclosure _ message: () -> String, _ error : NSError? = nil) {
-        if let error = error {
-            NSLog("%@%@ %@ with error %@", Tag, level.rawValue, message(), error);
-        } else {
-            NSLog("%@%@ %@", Tag, level.rawValue, message())
-        }
-    }
-    
-    static func debug(@autoclosure message: () -> String, _ error : NSError? = nil) {
-        #if DEBUG
-            log(.Debug, message, error)
-        #endif
-    }
-    
-    static func error(@autoclosure message: () -> String, _ error : NSError? = nil) {
-        log(.Error, message, error)
-    }
-    
-}
