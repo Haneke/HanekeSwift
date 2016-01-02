7f079daea137dc7d92e04de486f1769109d59704
Hermes Pique <hermespique@gmail.com>
2016-01-02 02:44:20 +0100

    Rename tests in NSURLResponse+HanekeTests

--- HanekeTests/NSURLResponse+HanekeTests.swift
+++ HanekeTests/NSURLResponse+HanekeTests.swift
@@ -13,54 +13,54 @@ class NSURLResponse_HanekeTests: XCTestCase {
     let httpURL = NSURL(string: "http://haneke.io")!
     let fileURL = NSURL(string: "file:///image.png")!
     
-    func testValidateLengthOfData_unknown() {
+    func testValidateLengthOfData_NSHTTPURLResponse_Unknown() {
         let response = NSHTTPURLResponse(URL: httpURL, statusCode: 200, HTTPVersion: "HTTP/1.1", headerFields: nil)!
         let data = NSData.dataWithLength(132)
         XCTAssertTrue(response.hnk_validateLengthOfData(data))
     }
     
-    func testValidateLengthOfData_Expected() {
+    func testValidateLengthOfData_NSHTTPURLResponse_Expected() {
         let length = 73
         let response = NSHTTPURLResponse(URL: httpURL, statusCode: 200, HTTPVersion: "HTTP/1.1", headerFields: ["Content-Length": String(length)])!
         let data = NSData.dataWithLength(length)
         XCTAssertTrue(response.hnk_validateLengthOfData(data))
     }
     
-    func testValidateLengthOfData_LessThanExpected() {
+    func testValidateLengthOfData_NSHTTPURLResponse_LessThanExpected() {
         let length = 73
         let response = NSHTTPURLResponse(URL: httpURL, statusCode: 200, HTTPVersion: "HTTP/1.1", headerFields: ["Content-Length": String(length)])!
         let data = NSData.dataWithLength(length - 10)
         XCTAssertFalse(response.hnk_validateLengthOfData(data))
     }
     
-    func testValidateLengthOfData_MoreThanExpected() {
+    func testValidateLengthOfData_NSHTTPURLResponse_MoreThanExpected() {
         let length = 73
         let response = NSHTTPURLResponse(URL: httpURL, statusCode: 200, HTTPVersion: "HTTP/1.1", headerFields: ["Content-Length": String(length)])!
         let data = NSData.dataWithLength(length + 10)
         XCTAssertTrue(response.hnk_validateLengthOfData(data))
     }
     
-    func testShouldValidateWhenLengthIsUndeterminedForNonHTTPResponse() {
+    func testValidateLengthOfData_NSURLResponse_Unknown() {
         let response = NSURLResponse(URL: fileURL, MIMEType: "image/png", expectedContentLength: -1, textEncodingName: nil)
         let data = NSData.dataWithLength(73)
         XCTAssertTrue(response.hnk_validateLengthOfData(data))
     }
     
-    func testShouldValidateWhenLengthIsCorrectForNonHTTPResponse() {
+    func testValidateLengthOfData_NSURLResponse_Expected() {
         let length = 73
         let response = NSURLResponse(URL: fileURL, MIMEType: "image/png", expectedContentLength: length, textEncodingName: nil)
         let data = NSData.dataWithLength(length)
         XCTAssertTrue(response.hnk_validateLengthOfData(data))
     }
     
-    func testShouldNotValidateWhenLengthIsLessThanExpectedForNonHTTPResponse() {
+    func testValidateLengthOfData_NSURLResponse_LessThanExpected() {
         let length = 73
         let response = NSURLResponse(URL: fileURL, MIMEType: "image/png", expectedContentLength: length, textEncodingName: nil)
         let data = NSData.dataWithLength(length - 10)
         XCTAssertFalse(response.hnk_validateLengthOfData(data))
     }
     
-    func testShouldStillValidateWhenLengthIsMoreThanExpectedForNonHTTPResponse() {
+    func testValidateLengthOfData_NSURLResponse_MoreThanExpected() {
         let length = 73
         let response = NSURLResponse(URL: fileURL, MIMEType: "image/png", expectedContentLength: length, textEncodingName: nil)
         let data = NSData.dataWithLength(length + 10)
