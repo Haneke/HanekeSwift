79fd85e2243a03876516e8ac2d7dc3b986189733
Dan <daniel.cech@gmail.com>
2016-05-18 14:18:51 +0200

    Update: Transition delay

--- Haneke/UIView+Haneke.swift
+++ Haneke/UIView+Haneke.swift
@@ -9,16 +9,16 @@
 import UIKit
 
 public extension HanekeGlobals {
-    
+
     public struct UIKit {
-        
+
         static func formatWithSize(size : CGSize, scaleMode : ImageResizer.ScaleMode, allowUpscaling: Bool = true) -> Format<UIImage> {
             let name = "auto-\(size.width)x\(size.height)-\(scaleMode.rawValue)"
             let cache = Shared.imageCache
             if let (format,_,_) = cache.formats[name] {
                 return format
             }
-            
+
             var format = Format<UIImage>(name: name,
                 diskCapacity: HanekeGlobals.UIKit.DefaultFormat.DiskCapacity) {
                     let resizer = ImageResizer(size:size,
@@ -32,17 +32,17 @@ public extension HanekeGlobals {
             }
             return format
         }
-        
+
         public struct DefaultFormat {
-            
+
             public static let DiskCapacity : UInt64 = 50 * 1024 * 1024
             public static let CompressionQuality : Float = 0.75
-            
+
         }
-        
-        static var SetImageAnimationDuration = 0.1
+
+        static var SetImageAnimationDuration = 0.5
         static var SetImageFetcherKey = 0
         static var SetBackgroundImageFetcherKey = 1
     }
-    
+
 }
