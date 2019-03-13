0d515338ecb656aeb706664cb201fa96ac41a8cc
Cihat Gündüz <commits@cihatguenduez.de>
2019-03-13 11:15:41 +0100

    Add basic SwiftPM manifest file

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:4.2
+import PackageDescription
+
+let package = Package(
+    name: "Haneke",
+    products: [
+        .library(name: "Haneke", targets: ["Haneke"])
+    ],
+    targets: [
+        .target(
+            name: "Haneke"
+        )
+    ]
+)
