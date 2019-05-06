1f27e3af52dc04af3e9e32ac1135603664f7ac43
Kevin Morton <kevnm67@gmail.com>
2019-05-06 07:34:43 -0400

    Fixed minor bug
    - Fixed minor bug (e.g. message() vs. message).

--- Haneke/Log.swift
+++ Haneke/Log.swift
@@ -27,7 +27,7 @@ struct Log {
     
     static func debug(message: @autoclosure () -> String, error: Error? = nil) {
         #if DEBUG
-            log(.Debug, message, error)
+            log(.Debug, message(), error)
         #endif
     }
     
